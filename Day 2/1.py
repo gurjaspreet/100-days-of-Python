948a3dc2af12ea05ec0295d9d28d1d871c702b21
gurjaspreet <107759543+gurjaspreet@users.noreply.github.com>
2024-08-30 23:20:37 +0530

    Day 2 complete :)

---
+++ Day 2/1.py
@@ -0,0 +1,5 @@
+a, b, c = 3, -4, 1 
+
+delta = b ** 2 - 4 * a * c
+
+print('Delta:', delta)
---
+++ Day 2/2.py
@@ -0,0 +1,6 @@
+a1 = 10 + 4 * 1 
+a10 = 10 + 4 * 10 
+
+s = (10 / 2) * (a1 + a10)
+
+print(f'The sum of the first 10 elements in a sequence: {s}')
