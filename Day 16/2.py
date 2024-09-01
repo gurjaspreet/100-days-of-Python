d79cb79588d8fc41fa2acc6bb878fa8028a255cf
gurjaspreet <107759543+gurjaspreet@users.noreply.github.com>
2024-09-01 16:11:58 +0530

    Day 16 completed :)

---
+++ Day 16/2.py
@@ -0,0 +1,20 @@
+tickers = [
+    'AAPL.US',
+    'AXP.US',
+    'BA.US',
+    'CAT.US',
+    'CSCO.US',
+    'CVX.US',
+    'DIS.US',
+    'DOW.US',
+    'GS.US',
+    'HD.US',
+    'IBM.US',
+    'INTC.US',
+]
+
+print(list(enumerate(tickers)))
+
+
+# l = [(i, tickers[i]) for i in range(len(tickers))]
+# print(l)
---
+++ Day 16/3.py
@@ -0,0 +1,16 @@
+tickers = [
+    'AAPL.US',
+    'AXP.US',
+    'BA.US',
+    'CAT.US',
+    'CSCO.US',
+    'CVX.US',
+    'DIS.US',
+    'DOW.US',
+    'GS.US',
+    'HD.US',
+    'IBM.US',
+    'INTC.US',
+]
+
+print(dict(enumerate(tickers)))
